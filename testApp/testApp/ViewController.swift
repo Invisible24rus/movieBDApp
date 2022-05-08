d9c48896c44ec7b396b25f0909b157752bab134b
Nikita <invisible24rus@gmail.com>
2022-05-08 13:40:21 +0300

    Create

---
+++ testApp/testApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by NIKITA NIKOLICH on 08.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
